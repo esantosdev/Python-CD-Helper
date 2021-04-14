b8d56893cdf25a8dfa8801bf67eca84b2bbfc8d8
Erika Santos <77362159+esantosdev@users.noreply.github.com>
2021-04-14 15:50:09 -0300

    Estilizando o background com imagem

--- mainHelper.py
+++ mainHelper.py
@@ -3,11 +3,15 @@ import os
 import Tkinter 
 import tkMessageBox
 from Tkinter import *
+from PIL import ImageTk, Image
 
 wd = Tkinter.Tk()
 wd.tk.call('wm','iconphoto',wd._w,Tkinter.PhotoImage(file="cs.png"))
 wd.title("Python CD Helper")
 wd.configure(bg='purple')
+img = ImageTk.PhotoImage(Image.open("cd.jpg"))
+panel= Label (wd, image=img)
+panel.pack(side = "bottom", fill = "both", expand = "yes")
 
 def executaScript():
     os.system('/home/eri/Documentos/scriptHelper.py')
