898d9157c54697495f0f6ea43e3f9ba2b3803775
Erika Santos <77362159+esantosdev@users.noreply.github.com>
2021-04-14 14:26:48 -0300

    First Commit

---
+++ mainHelper.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import Tkinter 
+import tkMessageBox
+from Tkinter import *
+
+wd = Tkinter.Tk()
+wd.tk.call('wm','iconphoto',wd._w,Tkinter.PhotoImage(file="cs.png"))
+wd.title("Python CD Helper")
+wd.configure(bg='purple')
+
+def executaScript():
+    os.system('/root/Desktop/scriptHelper.py')
+
+Bt = Tkinter.Button(wd,text="Abrir bandeja", command=executaScript, bg='aliceblue', height=10, width=100)
+Bt.pack()
+
+Bt.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+wd.mainloop()    
---
+++ scriptHelper.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from platform import system as platform_name
+from os import system
+import ctypes
+import time
+import tkinter
+import tkinter.messagebox
+import sys
+
+
+
+platforms_dictionary = {
+    "Windows": {                              #
+                "open" : 'ctypes.windll.WINMM.mciSendStringW(u"open L: type CDAudio alias L_drive", None, 0, None); ctypes.windll.WINMM.mciSendStringW(u"set L_drive door open", None, 0, None)',
+                "close": 'ctypes.windll.WINMM.mciSendStringW(u"open L: type CDAudio alias L_drive", None, 0, None); ctypes.windll.WINMM.mciSendStringW(u"set L_drive door closed", None, 0, None)'
+               },
+    "Darwin":  {
+                "open" : 'system("drutil tray open")',
+                "close": 'system("drutil tray closed")'
+               },
+    "Linux":   {
+                "open" : 'system("eject cdrom")',
+                "close": 'system("eject -t cdrom")'
+               },
+    "NetBSD":  {
+                "open" : 'system("eject cd")',
+                "close": 'system("eject -t cd")'
+               },
+    "FreeBSD": {
+                "open" : 'system("sudo cdcontrol eject")',
+                "close": 'system("sudo cdcontrol close")'
+               }
+}
+
+print(platform_name())
+
+if platform_name() in platforms_dictionary:
+    print("Abrindo")
+    exec(platforms_dictionary[platform_name()]["open"])
+    time.sleep(0.5)
+    print("Fechando")
+    time.sleep(0.5)
+    exec(platforms_dictionary[platform_name()]["close"])
+else:
+    print("S.O não suportado")
